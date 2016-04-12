67099db9ef30878bee8d2a057d366a1421d8da86
LiuEcho <lc7906@qq.com>
2016-04-12 18:50:17 +0800

    采购模板页

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,5 @@
 'use strict';
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ionic-timepicker'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -25,11 +17,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
   .state('tab', {
     url: '/tab',
@@ -45,21 +32,30 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     }
   })
-   .state('tab.main-plan', {
-    url: '/main/plan',
-    views: {
-      'tab-main': {
-        templateUrl: 'templates/main-plan.html',
-        controller: 'MainPlanCtrl'
-      }
-    }
-  })
   .state('tab.main-purchase', {
     url: '/main/purchase',
     views: {
       'tab-main': {
         templateUrl: 'templates/main-purchase.html',
         controller: 'MainPurchaseCtrl'
+      }
+    }
+  })
+  .state('tab.plan-new', {
+    url: '/main/plan/new',
+    views: {
+      'tab-main': {
+        templateUrl: 'templates/plan-new.html',
+        controller: 'PlanNewCtrl'
+      }
+    }
+  })
+   .state('tab.main-plan', {
+    url: '/main/plan',
+    views: {
+      'tab-main': {
+        templateUrl: 'templates/main-plan.html',
+        controller: 'MainPlanCtrl'
       }
     }
   })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,6 +27,41 @@ angular.module('starter.controllers', [])
     enableFriends: true
   }
 })  
+.controller('PlanNewCtrl', function ($scope, ionicTimePicker) {
+  //时间
+   $scope.startTime = new Date().getHours() + "时" + new Date().getMinutes() + "分";
+   $scope.endTime = new Date().getHours()+1 + "时" + new Date().getMinutes() + "分";
+  var ipObj1 = {
+    callback: function (val) {      //Mandatory
+      if (typeof (val) === 'undefined') {
+        $scope.time='未选择时间'
+      } else {
+        var selectedTime = new Date(val * 1000);
+        //console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+        $scope.startTime = selectedTime.getUTCHours() + '时' + selectedTime.getUTCMinutes() + '分';
+        $scope.endTime = selectedTime.getUTCHours()+1 + '时' + selectedTime.getUTCMinutes() + '分';
+      }
+    },
+    inputTime: (((new Date()).getHours() * 60 * 60) + ((new Date()).getMinutes() * 60)),
+    format: 24,         //Optional
+    step: 1,           //Optional
+    setLabel: '确定',
+    closeLabel:'关闭'    //Optional
+  };
+  $scope.openTimePicker = function(){
+    ionicTimePicker.openTimePicker(ipObj1);
+  }
+
+  //计数
+  $scope.num = 123;
+  $scope.add = function(){
+    $scope.num = $scope.num+1;
+  }
+  $scope.subtract = function(){
+    $scope.num = $scope.num+1;
+  }
+})
+
 .controller('MainPlanCtrl', function($scope,Purchases) {
   $scope.Purchases = Purchases.all();
 })
